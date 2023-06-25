0266cbe408bac60e31f8400ee6cce1f865744e81
tarooooooo <ryutaromaeda83@gmail.com>
2023-06-25 22:50:34 +0900

    add: 一覧表示

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, SafeAreaView, FlatList } from 'react-native';
+import { ListItem } from "./components/ListItem";
+import articles from "./dummies/articles.json";
 
 export default function App() {
+  const items = articles.map((article, index) => {
+    return <ListItem imageUrl={article.urlToImage} title={article.title} author={article.author} key={index.toString()} /> 
+  });
   return (
-    <View style={styles.container}>
-      <Text>Hello React Native!</Text>
+    <SafeAreaView style={styles.container}>
+      <FlatList 
+        data={articles}
+        renderItem={({ item }) => (
+          <ListItem 
+            imageUrl={item.urlToImage} 
+            title={item.title} 
+            author={item.author} 
+          /> 
+        )}
+        keyExtractor={(item, index) => index.toString()}
+      />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: '#eee',
+  }
 });
---
+++ components/ListItem.js
@@ -0,0 +1,56 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Image, View, Text } from 'react-native';
+
+export const ListItem = (props) => {
+  const { imageUrl, title, author } = props;
+  return (
+    <View style={styles.itemContainer}>
+        <View style={styles.leftContainer}>
+            <Image 
+                style={{width: 100, height: 100}}  
+                source={{uri: imageUrl}}
+            >
+            </Image>
+        </View>
+        <View style={styles.rightContainer}>
+            <Text numberOfLines={3} style={styles.text}>
+            {title}
+            </Text>
+            <Text style={styles.subText}>
+             {author}
+            </Text>
+        </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#eee',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  itemContainer: {
+    height: 100,
+    width: "100%",
+    backgroundColor: "white",
+    flexDirection: "row",
+    marginVertical: 5,
+  },
+  leftContainer: {
+    width: 100,
+  },
+  rightContainer: {
+    flex: 1,
+    padding: 10,
+    justifyContent: "space-between"
+  },
+  text: {
+    fontSize: 16
+  }, 
+  subText: {
+    fontSize: 12,
+    color: "gray"
+  }
+});
