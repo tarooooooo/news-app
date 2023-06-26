d32c32e7b0f0b29d7ac76896ae1fdc9d4f586c64
tarooooooo <ryutaromaeda83@gmail.com>
2023-06-26 20:47:51 +0900

    news apiから取得したデータを扱う&環境変数セット

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
+import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, SafeAreaView, FlatList } from 'react-native';
 import { ListItem } from "./components/ListItem";
-import articles from "./dummies/articles.json";
+import dummyArticles from "./dummies/articles.json";
+import axios from "axios";
+import Constants from 'expo-constants';
+
+const URL = `https://newsapi.org/v2/top-headlines?country=jp&category=business&apiKey=${Constants.manifest.extra.newsApiKey}`
 
 export default function App() {
-  const items = articles.map((article, index) => {
-    return <ListItem imageUrl={article.urlToImage} title={article.title} author={article.author} key={index.toString()} /> 
-  });
+  const [articles, setArticles] = useState([]);
+  const fetchArticles = async () => {
+    try {
+      const response = await axios.get(URL);
+      setArticles(response.data.articles);
+    } catch(error){
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchArticles();
+  }, []);
   return (
     <SafeAreaView style={styles.container}>
       <FlatList 
